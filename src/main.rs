44b209eddd2d7da4717be33a112569d6761bfc9e
Rivo the Red <190857114+rivothered@users.noreply.github.com>
2024-12-12 11:50:04 -0300

    feat: reading data from terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+    // Variaveis sao imutaveis por padrao
+    // e necessario declarar como mutavel (mut)
+    // para alterar seu valor
+
+    // Usando a instancia de Stdin fornecida por
+    // std::io::stdin() para ler dados inseridos no terminal
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    // O metodo expect finaliza o programa e exibe a
+    // mensagem de erro informada caso o resultado
+    // retornado seja o enum Result<Error>
+
+    println!("You guessed: {}", guess);
 }
