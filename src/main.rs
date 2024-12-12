432408f3af23486a38615c9c3ce0b0e469ec8c46
Rivo the Red <190857114+rivothered@users.noreply.github.com>
2024-12-12 12:09:48 -0300

    feat: added random number generation

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ fn main() {
     // Gerando um numero aleatorio
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess");
 
     let mut guess = String::new();
