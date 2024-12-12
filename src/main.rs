2c5986967657414657b87022a1052176058aef25
Rivo the Red <190857114+rivothered@users.noreply.github.com>
2024-12-12 12:23:17 -0300

    feat: comparison between generated number and guessed number

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     // Gerando um numero aleatorio
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
 
     println!("Please input your guess");
 
@@ -25,5 +24,14 @@ fn main() {
     // mensagem de erro informada caso o resultado
     // retornado seja o enum Result<Error>
 
+    // Convertendo String para u32
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
